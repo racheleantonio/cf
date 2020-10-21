4d1ac40376037c9c7d66ef2c273f9c4db9c8644a
unknown <racheleantonio@outlook.com>
2020-10-21 16:21:15 +0200

    favicon and animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './styles/tailwind.css'
 function App() {
   return (
     <div className="h-full bg-gray-900 flex justify-center items-center p-8">
-      <header className="text-3xl lg:text-6xl text-center text-gray-400">
+      <header className=" fadeIn text-3xl lg:text-6xl text-center text-gray-400">
        
           Nothing to see here.
   
