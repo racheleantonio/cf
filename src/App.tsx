b416bfa4e612433d98f491819d0e2023ca6820f2
unknown <racheleantonio@outlook.com>
2020-10-31 16:34:05 +0100

    add cv

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ReactComponent as Linkedin } from "./assets/Linkedin.svg";
 import { ReactComponent as Github } from "./assets/Github.svg";
 import { ReactComponent as Twitter } from "./assets/Twitter.svg";
 import { ReactComponent as Mail } from "./assets/Mail.svg";
-import { ReactComponent as Telegram } from "./assets/Telegram.svg";
 import { ReactComponent as CV } from "./assets/CV.svg";
 import { ReactComponent as Skype } from "./assets/Skype.svg";
 
@@ -23,10 +22,10 @@ function App() {
         <a href='https://www.linkedin.com/in/racheleantonio/' target='blank'><Linkedin className="h-24 w-24 m-3 fadeIn1 opacity-0" /></a>
           <a href='https://github.com/racheleantonio' target='blank'><Github className="h-24 w-24 m-3 fadeIn2 opacity-0" /></a>
           <a href='https://twitter.com/RacheleAntonio_' target='blank'><Twitter className="h-24 w-24 m-3 fadeIn3 opacity-0" /></a>
-          {/* <a href='./assets/cv.pdf' target='blank'><CV className="h-24 w-24 m-3 fadeIn4 opacity-0" /></a> */}
+          <a href={`${process.env.PUBLIC_URL}/cv.pdf`} target='blank'><CV className="h-24 w-24 m-3 fadeIn4 opacity-0" /></a>
           <a href='mailto:racheleantonio@outlook.com' target='blank'><Mail className="h-24 w-24 m-3 fadeIn4 opacity-0" /></a>
           <a href='skype:racheleantonio_2?chat' target='blank'><Skype className="h-24 w-24 m-3 fadeIn5 opacity-0" /></a>
-          <a href='https://t.me/AntoRack' target='blank'><Telegram className="h-24 w-24 m-3 fadeIn6 opacity-0" /></a>
+          {/* <a href='https://t.me/AntoRack' target='blank'><Telegram className="h-24 w-24 m-3 fadeIn6 opacity-0" /></a> */}
         </div>
       </div>
     </div>
