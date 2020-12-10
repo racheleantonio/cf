807261ecddc6773c377fd2a29d663ed9b91bce96
unknown <racheleantonio@outlook.com>
2020-12-10 02:56:48 +0100

    neumorphism

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/tailwind.css";
-import { ReactComponent as Linkedin } from "./assets/Linkedin.svg";
-import { ReactComponent as Github } from "./assets/Github.svg";
-import { ReactComponent as Twitter } from "./assets/Twitter.svg";
-import { ReactComponent as Mail } from "./assets/Mail.svg";
-import { ReactComponent as CV } from "./assets/CV.svg";
-import { ReactComponent as Skype } from "./assets/Skype.svg";
+import { ReactComponent as Linkedin } from "./assets/full/linkedin.svg";
+import { ReactComponent as Github } from "./assets/full/github.svg";
+import { ReactComponent as Twitter } from "./assets/full/twitter.svg";
+import { ReactComponent as Mail } from "./assets/full/mail.svg";
+import { ReactComponent as CV } from "./assets/full/cv.svg";
+import { ReactComponent as Skype } from "./assets/full/skype.svg";
 import { ReactComponent as RA } from "./assets/RA.svg";
 
 // @ts-ignore
-import Tilt from "react-parallax-tilt";
+import div from "react-parallax-div";
 
 function App() {
   return (
@@ -22,43 +22,43 @@ function App() {
           Antonio
         </header>
         <div className="mt-6 flex flex-wrap w-full justify-center items-center">
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          <div className="rounded-full h-24 w-24 m-3 neu flex flex-wrap justify-center items-center fadeIn1 opacity-0">
             <a
               href="https://www.linkedin.com/in/racheleantonio/"
               target="blank"
             >
-              <Linkedin className="h-24 w-24 m-3 fadeIn1 opacity-0" />
+              <Linkedin className="h-12 w-12 fadeIn1 opacity-0" />
             </a>
-          </Tilt>
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          </div>
+          <div className="rounded-full h-24 w-24 m-3 neu flex flex-wrap justify-center items-center fadeIn2 opacity-0">
             <a href="https://github.com/racheleantonio" target="blank">
-              <Github className="h-24 w-24 m-3 fadeIn2 opacity-0" />
+              <Github className="h-12 w-12 fadeIn2 opacity-0" />
             </a>
-          </Tilt>
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          </div>
+          <div className="rounded-full h-24 w-24 m-3 neu flex flex-wrap justify-center items-center fadeIn3 opacity-0">
             <a href="https://twitter.com/RacheleAntonio_" target="blank">
-              <Twitter className="h-24 w-24 m-3 fadeIn3 opacity-0" />
+              <Twitter className="h-12 w-12 fadeIn3 opacity-0" />
             </a>
-          </Tilt>
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          </div>
+          <div className="rounded-full h-24 w-24 m-3 neu flex flex-wrap justify-center items-center fadeIn4 opacity-0">
             <a href={`${process.env.PUBLIC_URL}/cv.pdf`} target="blank">
-              <CV className="h-24 w-24 m-3 fadeIn4 opacity-0" />
+              <CV className="h-12 w-12 fadeIn4 opacity-0" />
             </a>
-          </Tilt>
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          </div>
+          <div className="rounded-full h-24 w-24 m-3 neu flex flex-wrap justify-center items-center fadeIn4 opacity-0">
             <a href="mailto:racheleantonio@outlook.com" target="blank">
-              <Mail className="h-24 w-24 m-3 fadeIn4 opacity-0" />
+              <Mail className="h-12 w-12 fadeIn4 opacity-0" />
             </a>
-          </Tilt>
-          <Tilt perspective={100} tiltReverse={true} gyroscope={true}>
+          </div>
+          <div className="rounded-full h-24 w-24 neu  m-3 flex flex-wrap justify-center items-center fadeIn5 opacity-0">
             <a href="skype:racheleantonio_2?chat" target="blank">
-              <Skype className="h-24 w-24 m-3 fadeIn5 opacity-0" />
+              <Skype className="h-12 w-12 fadeIn5 opacity-0" />
             </a>
-          </Tilt>
-          {/* <Tilt><a href='https://t.me/AntoRack' target='blank'><Telegram className="h-24 w-24 m-3 fadeIn6 opacity-0" /></a> */}
+          </div>
+          {/* <div><a href='https://t.me/AntoRack' target='blank'><Telegram className="h-24 w-24 m-3 fadeIn6 opacity-0" /></a> */}
         </div>
       </div>
-      <div className="p-4 absolute bottom-0 right-0">
+      <div className="p-3 md:p-4 absolute bottom-0 right-0">
         <RA className="items-end h-8 w-8 m-3 fadeIn6 opacity-0" />
       </div>
     </div>
