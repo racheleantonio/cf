60ca6374966afed889d0d6f2b55db62d2d587cbd
unknown <racheleantonio@outlook.com>
2021-02-13 23:21:50 +0100

    Updates

---
+++ precache-manifest.00c0bb99f085e31b25c20009578116ec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843c3ca6c1501a356d8a968d6fb5cd8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86bb14780e51d7662a0d",
+    "url": "/static/css/main.d81d98dd.chunk.css"
+  },
+  {
+    "revision": "288d226a9c3f5a354798",
+    "url": "/static/js/2.91c3e3df.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.91c3e3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bb14780e51d7662a0d",
+    "url": "/static/js/main.27d0d095.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "fc5155566791b6918a8c70d7c9212666",
+    "url": "/static/media/Gilmer Bold.fc515556.otf"
+  },
+  {
+    "revision": "c440b3aadf988f2d64593dc4da370b98",
+    "url": "/static/media/RA.c440b3aa.svg"
+  },
+  {
+    "revision": "8e8cdf64ad2e055afff740d7204fca58",
+    "url": "/static/media/cv.8e8cdf64.svg"
+  },
+  {
+    "revision": "31f52670f42ad38c8b6f8aefdf9dac23",
+    "url": "/static/media/github.31f52670.svg"
+  },
+  {
+    "revision": "d1c53651cd36f8f90ab175856fb98a3f",
+    "url": "/static/media/linkedin.d1c53651.svg"
+  },
+  {
+    "revision": "704cef506b9fff2849948a8b400f9b40",
+    "url": "/static/media/mail.704cef50.svg"
+  },
+  {
+    "revision": "204e29c20609830c6a42b15259ff8969",
+    "url": "/static/media/skype.204e29c2.svg"
+  },
+  {
+    "revision": "40ea2906aef56e95b547308eb7a56ffd",
+    "url": "/static/media/twitter.40ea2906.svg"
+  }
+]);
