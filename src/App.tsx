99b29584e7a8e1e6eb08ddef477d93a1d83620c9
Antonio Rachele <racheleantonio@outlook.com>
2024-01-04 15:50:56 +0100

    Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,61 @@ import { ReactComponent as CV } from "./assets/full/resume.svg";
 import { ReactComponent as Telegram } from "./assets/full/telegram.svg";
 import { ReactComponent as RA } from "./assets/RA.svg";
 import { Tile } from "./components/Tile";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 const data = [
-  { icon: Linkedin, link: "https://www.linkedin.com/in/racheleantonio/" },
-  { icon: Github, link: "https://github.com/racheleantonio" },
-  { icon: Twitter, link: "https://twitter.com/RacheleAntonio_" },
-  { icon: Mail, link: "mailto:racheleantonio@outlook.com" },
-  { icon: CV, link: "./cv.pdf" },
-  // { icon: Skype, link: "skype:racheleantonio_2?chat" },
-  { icon: Telegram, link: "https://t.me/AntoRack" },
+  {
+    name: "Linkedin",
+    icon: Linkedin,
+    link: "https://www.linkedin.com/in/racheleantonio/",
+  },
+  { name: "Github", icon: Github, link: "https://github.com/racheleantonio" },
+  {
+    name: "Twitter",
+    icon: Twitter,
+    link: "https://twitter.com/RacheleAntonio_",
+  },
+  { name: "Mail", icon: Mail, link: "mailto:racheleantonio@outlook.com" },
+  { name: "CV", icon: CV, link: "./cv.pdf" },
+  // { name:'Skype',icon: Skype, link: "skype:racheleantonio_2?chat" },
+  { name: "Telegram", icon: Telegram, link: "https://t.me/AntoRack" },
 ];
 
-const App = () => (
-  <div className="h-full w-full bg-gray-900 flex justify-center items-center p-8">
-    <div className="mt-3 mb-8">
-      <header className="text-white opacity-75 zoomIn1 text-5xl tracking-wider select-none text-center">
-        Rachele
-      </header>
-      <header className="text-white opacity-75 zoomIn2 text-5xl tracking-wider lg:mt-3 select-none text-center">
-        Antonio
-      </header>
-      <div className="mt-10 grid grid-cols-3 gap-8 ">
-        {data.map(({ icon: Name, link }, index) => (
-          <Tile className={`opacity-0 fade-in-${index}`} link={link}>
-            <Name className="h-12 w-12" />
-          </Tile>
-        ))}
+const App = () => {
+  useEffect(() => {
+    ReactGA.initialize("G-FE3M24CTLB");
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
+  return (
+    <div className="h-full w-full bg-gray-900 flex justify-center items-center p-8">
+      <div className="mt-3 mb-8">
+        <header className="text-white opacity-75 zoomIn1 text-5xl tracking-wider select-none text-center">
+          Rachele
+        </header>
+        <header className="text-white opacity-75 zoomIn2 text-5xl tracking-wider lg:mt-3 select-none text-center">
+          Antonio
+        </header>
+        <div className="mt-10 grid grid-cols-3 gap-8 ">
+          {data.map(({ name, icon: Icon, link }, index) => (
+            <Tile
+              key={index}
+              name={name}
+              className={`opacity-0 fade-in-${index}`}
+              link={link}
+            >
+              <Icon className="h-12 w-12" />
+            </Tile>
+          ))}
+        </div>
+      </div>
+      <div className="p-3 md:p-4 absolute bottom-0 right-0">
+        <RA className="items-end h-8 w-8 m-3 fade-in-5 opacity-0" />
       </div>
     </div>
-    <div className="p-3 md:p-4 absolute bottom-0 right-0">
-      <RA className="items-end h-8 w-8 m-3 fade-in-5 opacity-0" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,21 +1,36 @@
 import { ReactNode } from "react";
+import ReactGA from "react-ga";
 
 export const Tile = ({
+  name,
   className,
   link,
   children,
 }: {
+  name: string;
   className?: string;
   link: string;
   children: ReactNode;
-}) => (
-  <a
-    href={link}
-    target="blank"
-    className={`rounded-2xl h-24 w-24 neu grid place-content-center ${
-      className ?? ""
-    }`}
-  >
-    {children}
-  </a>
-);
+}) => {
+  const trackButtonClick = () => {
+    console.log(name);
+    ReactGA.event({
+      category: "Tile Click",
+      action: name,
+      label: name,
+    });
+  };
+  return (
+    <a
+      aria-label={name}
+      href={link}
+      target="blank"
+      className={`rounded-2xl h-24 w-24 neu grid place-content-center ${
+        className ?? ""
+      }`}
+      onClick={trackButtonClick}
+    >
+      {children}
+    </a>
+  );
+};
